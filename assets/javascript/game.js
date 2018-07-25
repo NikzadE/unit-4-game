2070f69489c6e9ae1da86a325c2810f874f13c92
NikzadE <arshad.nikzad@gmail.com>
2018-07-25 14:06:58 -0400

    updating styles

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+console.log ("Ready to go!")
+//Declaring global variables
+var randomNumber;
+var losses;
+var wins;
+
+for (var i=0; i<4; i++){
+    var crystal = $("<div>")
+        crystal.attr("class", "crystal")
+    $(".crystals").append(crystal);
+
+    console.log ("4 times??")
+}
+
+
+
+//game with 4 crystal and a random result between 19-120
+
+//every crystal has a random number between 1-12
+
+//number should be generated every single time we win or lose
+
+//everytime won new number should be generated to crystal
+
+//when we click any crystal it should be adding the previous result until it hits the total score
+
+// if it is greater than randomNumber we start over --- we lost and increment a loss counter
+
+//if equal to randomNumber we increment a win counter 
+
+
